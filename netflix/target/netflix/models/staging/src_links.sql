ea0d3f1cc13acab02140061550cfb8bd83d2cacf
Abhishek Rajendra Harde <73383047+abhiharde@users.noreply.github.com>
2026-07-02 21:03:46 +0530

    Delete netflix/target/netflix directory

--- netflix/target/netflix/models/staging/src_links.sql
+++
@@ -1,8 +0,0 @@
-WITH raw_links AS (
-    SELECT * FROM MOVIELENS.RAW.RAW_LINKS
-)
-SELECT
-    movieID AS movie_id,
-    imdbID AS imdb_id,
-    tmdbID AS tmdb_id
-FROM raw_links
--- netflix/target/netflix/models/staging/src_movies.sql
+++
@@ -1,8 +0,0 @@
-WITH raw_movies AS (
-    SELECT * FROM {{ source('netflix', 'r_movies') }}
-)
-SELECT
-    movieID as movie_id,
-    title,
-    genres
-FROM raw_movies
