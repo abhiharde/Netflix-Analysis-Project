e9ab3187eb5bb6c3daef1f17299895fc6ac5a790
Abhishek Rajendra Harde <73383047+abhiharde@users.noreply.github.com>
2026-07-02 19:46:56 +0530

    Uploaded other staging models
    NA

---
+++ netflix/models/staging/src_tags.sql
@@ -0,0 +1,13 @@
+{{config(
+    materialized='table'
+)}}
+
+WITH RAW_TAGS AS (
+    SELECT * FROM MOVIELENS.RAW.RAW_TAGS
+)
+SELECT
+    userID AS user_id,
+    movieID AS movie_id,
+    tag,
+    TO_TIMESTAMP_LTZ(timestamp) AS tag_timestamp
+FROM RAW_TAGS
